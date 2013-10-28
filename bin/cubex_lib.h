5375d4755a4ce12ad7aec4011d8b0f3bba62fbaf
feiranchen <fc254@cornell.edu>
2013-10-27 21:42:34 -0400

    added things

---
+++ bin/cubex_lib.h
@@ -0,0 +1,17 @@
+typedef struct iter {
+	void* value = NULL;
+	int size;
+} Iterable;
+
+typedef struct int {
+	int value;
+} Integer;
+
+typedef struct string {
+	void* value = NULL;
+	int size;
+} String;
+
+typedef struct boolean {
+	int value;
+} Boolean;
