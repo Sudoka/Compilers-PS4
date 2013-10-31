1a7a2a93b504db2646ca019765aea16bd6b3fb3b
yinglei <yw437@cornell.edu>
2013-10-31 05:53:50 -0400

    merging

--- src/Cubex.java
+++ src/Cubex.java
@@ -36,8 +36,5 @@ public class Cubex {
 		//	System.exit(-2);
 		//}
 		System.out.println("accept");
-
-		ArrayList<String> localVars = new ArrayList<String>();
-		System.out.print(ourProgram.toC(localVars));
 	}
 }
