35244d9574412f0ce6dcd371758ad4cbd98cd714
yinglei <yw437@cornell.edu>
2013-11-04 05:40:46 -0500

    added cstr mapping

--- src/CuStat.java
+++ src/CuStat.java
@@ -36,6 +36,7 @@ class AssignStat extends CuStat{
 	@Override public String toC(ArrayList<String> localVars) {
 		String exp_toC = ee.toC(localVars);
 		Helper.cVarType.put(var.toString(), ee.getCastType());
+		Helper.cStr.put(var.toString(), ee.getHelperHead());
 		super.ctext ="\n\n\n";
 		super.ctext += ee.construct();
 		//the below sentence can be removed by higher level blocks
