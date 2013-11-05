b46c7ad18edc40afe495aa6ac35b5d5c6024ff80
feiranchen <fc254@cornell.edu>
2013-11-05 17:57:45 -0500

    class fix

--- src/Helper.java
+++ src/Helper.java
@@ -13,6 +13,7 @@ public class Helper {
 	protected static HashMap<String, String> cVarType = new HashMap<String, String>();
 	protected static HashMap<String, String> iterType = new HashMap<String, String>();
 	protected static HashMap<String, String> cFunType = new HashMap<String, String>();
+	protected static HashMap<String, List<String>> funGenType = new HashMap<String, List<String>>();
 	protected static ArrayList<String>		 funArgList=new ArrayList<String>();
 	protected static HashMap<String, String> cClassSuper=new HashMap<String, String>();
 	protected static HashMap<String, String> cStr = new HashMap<String, String>();
